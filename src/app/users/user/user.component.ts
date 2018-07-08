430e8a54721035fae93c6f632000ee1186624e41
techraiders <hello_navneet@hotmail.com>
2018-07-08 06:26:09 -0700

    Subscribe to changes in ActivatedRoute params to reflect changes when updating params from inside same component.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -12,10 +12,21 @@ export class UserComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    /* If we are already on the same route, Angular doesn't change
+    the route, so snapshot og params isn't updated. */
     this.user = {
       id: this.route.snapshot.params.id,
       name: this.route.snapshot.params.name
     };
+    
+    /* Watches changes in params and executes handlers */
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params.id;
+          this.user.name = params.name;
+        }
+      );
   }
 
 }
