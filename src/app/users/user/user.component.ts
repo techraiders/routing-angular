c06537951d723bf34db69d6ea73f7b4561e50a8f
techraiders <hello_navneet@hotmail.com>
2018-07-08 06:37:02 -0700

    Manually unsubscribes from params observables onDestroy.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -20,13 +22,21 @@ export class UserComponent implements OnInit {
     };
     
     /* Watches changes in params and executes handlers */
-    this.route.params
+    this.paramsSubscription = this.route.params
       .subscribe(
         (params: Params) => {
           this.user.id = params.id;
           this.user.name = params.name;
         }
       );
+  }
+
+  ngOnDestroy () {
+    /* This is the observable of angular, angular takes care to unsubscribe
+    from this when component is destroyed, it is not essential to unsubscribe
+    here manually, but it is super important when you create your own observable.
+    */
+    this.paramsSubscription.unsubscribe();
   }
 
 }
