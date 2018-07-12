236866143ec153739afed08aa7b6c8e522d5de0d
techraiders <hello_navneet@hotmail.com>
2018-07-12 22:30:17 +0530

    Protects server route, implementing CanActivate interface of angular/router, creates authGuard and AuthService.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 
 
 @NgModule({
@@ -32,7 +34,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
