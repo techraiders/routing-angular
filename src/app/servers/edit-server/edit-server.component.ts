3c688555683b2cce757f7998709bc31a01750013
techraiders <hello_navneet@hotmail.com>
2018-07-15 20:26:25 +0530

    Implements canDeactivateGuard which confirm routes changes if unsaved data.

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Observable} from 'rxjs/observable';
 import { ServersService } from '../servers.service';
 
 @Component({
@@ -12,9 +13,11 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changesSaved = false;
 
   constructor(private serversService: ServersService,
-    private route : ActivatedRoute) { }
+    private route : ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
@@ -26,14 +29,25 @@ export class EditServerComponent implements OnInit {
       }
     );
     this.route.fragment.subscribe();
-
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params.id;
+    this.server = this.serversService.getServer(id);
+    // Subscribe to params here , if params changes
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 
+  canDeactivate () : Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit) return true;
+    if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved) {
+      return confirm('Do you want to discard the changes?');
+    } else {
+      return true;
+    }
+  }
 }
